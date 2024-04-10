0ea86ae6491e4e5153f38e5bae05a12b1c710b28
iTsGiuse <giuse.amato@icloud.com>
2024-04-10 16:08:47 +0200

    feat: add image

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script>
-import AppHeader from './components/header/AppHeader.vue'
 
-export default {
-    components: {
-      AppHeader,
+  import AppHeader from './components/header/AppHeader.vue';
+  import AppMain from './components/main/AppMain.vue';
+
+  export default {
+      components: {
+        AppHeader,
+        AppMain,
+      }
     }
-  }
+
 </script>
 
 <template>
+  
     <AppHeader></AppHeader>
+    <AppMain></AppMain>
 
 </template>
 
---
+++ src/components/main/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        name: 'AppMain',
+    }
+</script>
+
+<template>
+
+    <div class="container">
+        <div class="card-image">
+            <img src="../assets/img/nitro.jpeg" alt="Foto">
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+    .container{
+        width: 800px;
+        margin: auto;
+        margin-top: 20px;
+    }
+    .card-image{
+        width: 50px;
+        height: 50px;
+    }
+    img{
+        width: 100%;
+    }
+
+</style>
