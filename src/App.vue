c7b2b2dac4a1521df42230ca75a43d4701a609bd
iTsGiuse <giuse.amato@icloud.com>
2024-04-10 15:41:53 +0200

    feat: add title

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AppHeader from './components/header/AppHeader.vue'
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name: 'AppHeader'
+    }
+</script>
+
+<template>
+    <header>
+        <h1>La mia prima app con Vite!</h1>
+    </header>
+</template>
+
+<style scoped>
+    header{
+        height: 70px;
+        text-align: center;
+    }
+</style>
